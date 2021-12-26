5b7f7c5719f22e507b78df3889f4260a8d51b430
Biraj Mainali <biraj.prathamit@gmail.com>
2021-12-26 22:42:45 +0545

    feat : stuff

--- src/FileManager.Domain/ValueObject/OrganizationUserVo.cs
+++ src/FileManager.Domain/ValueObject/OrganizationUserVo.cs
@@ -1,10 +1,11 @@
 ﻿using FileManager.Domain.Dto;
 
-namespace FileManager.Domain.ValueObject;
-
-public class OrganizationUserVo : OrganizationDto
+namespace FileManager.Domain.ValueObject
 {
-    public string Name { get; set; }
-    public string Gender { get; set; }
-    public string Password { get; set; }
+    public class OrganizationUserVo : OrganizationDto
+    {
+        public string Name { get; set; }
+        public string Gender { get; set; }
+        public string Password { get; set; }
+    }
 }
